cd2af906858a5c7a3b00bf80e03f90abd58a5222
Dennis Hunter <27469074-TerrestrialEntity@users.noreply.gitlab.com>
2026-05-11 10:19:21 +0200

    tff cascaded -> div16

--- digital/div16/README.md
+++ digital/div16/README.md
@@ -14,10 +14,10 @@ RO -> buffer -> DFF1 -> DFF2 -> DFF3 -> DFF4
                 /2      /4      /8      /16
 ```
 
-## T-Flipflop
+## div16: T-Flipflop Cascade
 
 
-Compiling/synthesizing the ``tff`` module, inside ``octs_testdesign/digital/tff`` do:
+Compiling/synthesizing the ``div16`` module, inside ``octs_testdesign/digital/div16`` do:
 ```sh
 librelane config.yaml
 ```
@@ -26,15 +26,16 @@ librelane config.yaml
 
 Simulating the testbench using ``iverilog``:
 ```sh
-iverilog -g2012 -o srv/tff_tb.out srv/tff.sv srv/tff_tb.sv
+iverilog -g2012 -o sim/div16_tb.out src/div16.sv src/div16_tb.sv
+vvp sim/div16_tb.out
 ```
 
 > [!IMPORTANT]
 > The option ``-g2012`` tells ``iverilog`` to read the input file as SystemVerilog.
 
-This produces the files ``tff_tb.out`` and ``tff_tb.vcd`` inside ``src/`` and prints something like a truth table to the terminal. Viewing the waveforms with ``GTKWave``:
+This produces the files ``div16_tb.out`` and ``div16_tb.vcd`` inside ``src/`` and prints something like a truth table to the terminal. Viewing the waveforms with ``GTKWave``:
 ```sh
-gtkwave src/tff_tb.vcd
+gtkwave sim/div16_tb.vcd
 ```
 
 ![[assets/waveform.png]]
---
+++ digital/div16/src/div16_tb.sv
@@ -0,0 +1,51 @@
+`timescale 1ns/1ps
+
+module div16_tb;
+
+    // Testbench signals
+    logic clk_in;
+    logic meas_en;
+    logic q0;
+    logic q1;
+    logic q2;
+    logic q3;
+
+    // Instantiate DUT = Device Under Test
+    div16 dut (
+        .clk_in (clk_in),
+        .meas_en (meas_en),
+        .q0 (q0),
+        .q1 (q1),
+        .q2 (q2),
+        .q3 (q3)
+    );
+
+    // Clock generation:
+    // Toggle every 2.5 ns -> full period = 5 ns
+    initial begin
+        clk_in = 0;
+        forever #2.5 clk_in = ~clk_in; // changes every 2.5ns
+    end
+
+    // Stimulus:
+    // Apply reset, then release it, then let it run
+    initial begin
+        meas_en = 1;           // hold meas_en active
+        #12;                   // wait 12ns
+        meas_en = 0;           // release meas_en
+        #100;                // let simulation run for 100ns
+        $finish;
+    end
+
+    // Simple monitoring in console
+    initial begin
+        $display("time\tclk_in\tmeas_en\tq0\tq1\tq2\tq3");
+        $monitor("%0t\t%b\t%b\t%b\t%b\t%b\t%b", $time, clk_in, meas_en, q0, q1, q2, q3);
+    end
+
+    initial begin
+        $dumpfile("sim/div16_tb.vcd");
+        $dumpvars(0, div16_tb);
+    end
+
+endmodule
